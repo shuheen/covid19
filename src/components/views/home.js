6988b078e9422f86d0dd93a929c5b7a516fb8861
Unknown <mail.shuaibkhan@gmail.com>
2020-03-28 22:50:07 +0530

    Fetch API

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -5,9 +5,33 @@ import {
 } from 'react-bootstrap';
 import TopWidget from './topWidget';
 export class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data:[]
+        }
+    }
+
+    componentDidMount(){
+        fetch("https://covid-193.p.rapidapi.com/statistics", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "covid-193.p.rapidapi.com",
+                "x-rapidapi-key": "63600dc988msh87da34cf0d7b124p14ce99jsnb331277689b3"
+            }
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+
+
     render() {
         return (
-            <div class="main-wrapper">
+            <div className="main-wrapper">
                 <Container>
                     <Row>
                         <Col md={3} lg={3}>
