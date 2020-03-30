4aeb1d58c2a9e98547d660c9bfb3dc9d0936f278
shuoor <mail.shuaibkhan@gmail.com>
2020-03-30 17:56:10 +0530

    Maps changes

--- src/components/views/searchbox.js
+++ src/components/views/searchbox.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Select2 from 'react-select2-wrapper';
+// import Select2 from 'react-select2-wrapper';
 import {
     Col,Row
 } from 'react-bootstrap';
@@ -7,15 +7,7 @@ function SearchBox() {
     return (
         <Row>
             <Col md={12}>
-                <Select2 data={[
-    { text: 'bug', id: 1 },
-    { text: 'feature', id: 2 },
-    { text: 'documents', id: 3 },
-    { text: 'discussion', id: 4 },
-  ]}
-  options={{
-    placeholder: 'search by tags',
-  }} />
+                
             </Col>
         </Row>
     )
